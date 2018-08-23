83958f69d4a0dada3d0e6a0b6420dd4eb877a6e6
Felipe Sousa Pessina <ra171214@rabugento.lab.ic.unicamp.br>
2018-08-23 15:01:54 -0300

    finish list exercises 2

--- mc346/class-2/execises.hs
+++ mc346/class-2/execises.hs
@@ -1,6 +1,31 @@
 
 pos element l = pos' element l 1 where
-                     pos' element [] acc = []
-                     pos' element (x:xs) acc = if x == element
-                     then acc:pos' element xs (acc+1)
-                     else pos' element xs (acc+1)
+  pos' element [] acc = []
+  pos' element (x:xs) acc = if x == element
+    then acc:pos' element xs (acc+1)
+    else pos' element xs (acc+1)
+
+split l e  = [(lista1 l e)] ++ [(lista2 l e)] where
+  lista1 (x:xs) e
+    | x == e = []
+    | otherwise = x:lista1 xs e
+  lista2 (x:xs) e
+    | x == e = xs
+    | otherwise = lista2 xs e
+
+splitall [] e = []
+splitall l e = [(lista1 l e)] ++ splitall (lista2 l e) e where
+  lista1 (x:xs) e
+    | x == e = []
+    | otherwise = x:lista1 xs e
+  lista2 (x:xs) e
+    | x == e = xs
+    | otherwise = lista2 xs e
+
+dropn l n = dropn' l n 1 where
+  dropn' [] n acc = []
+  dropn' (x:xs) n acc = if acc < n then (dropn' xs n (acc+1)) else xs
+
+taken l n = taken' l n 0 where
+  taken' [] n acc = []
+  taken' (x:xs) n acc = if acc < n then x:(taken' xs n (acc+1)) else []
