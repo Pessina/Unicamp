a0fde41748f6a181d36d0f56397559cc72b5c39b
Joker <fs.pessina@gmail.com>
2018-10-06 18:30:28 -0300

    add new functions

--- mc346/prolog/project/proj2.pl
+++ mc346/prolog/project/proj2.pl
@@ -16,7 +16,11 @@ toTrechos(Input, Trechos) :- atomic_list_concat(TrechosString, '\n', Input),  ma
 
 % Take the list of trechos, and return the segments
 getSegments([], _, []).
-getSegments([X|XS], Trechos, Segments) :- getSegments(XS, Trechos, SegmentsR), matchTrecho(X, Trechos, MatchList), Segments is [MatchList|SegmentsR].
+getSegments([X|XS], Trechos, Segments) :- getSegments(XS, Trechos, SegmentsR), getXSegments(X, Trechos, MatchList), Segments is [MatchList|SegmentsR].
 
-matchTrecho(X, [], []).
-matchTrecho
+getXSegments(X, [], []).
+getXSegments(X, [Y|YS], MatchList) :- getXSegments(X, YS, MatchListR), matchTrecho(X, Y, Segment, 0, YInicio), Segment != [] -> MatchList is [Segment|MatchListR]; MatchListR.
+
+matchTrecho([], YS, Segment, Acc, YInicio) :- Acc >= 4 -> Segment is YS; Segment is false.
+matchTrecho([X|XS], [Y|YS], Segment, Acc, YInicio) :- X = Y, Acc is Acc + 1, matchTrecho(XS, YS, SegmentR, Acc), SegmentR = false -> Segment is false; Segment is [X|SegmentR].
+matchTrecho([X|XS], [Y|YS], Segment, Acc, YInicio) :- Acc is 0, matchTrecho(XS, YInicio, SegmentR, Acc), SegmentR = false -> Segment is false; Segment is [X|SegmentR].
