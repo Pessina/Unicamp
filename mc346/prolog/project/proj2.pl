47e2474eecc07e67296395479ffc570971ac4294
Felipe <fs.pessina@gmail.com>
2018-10-07 20:14:15 -0300

    fix some bugs

--- mc346/prolog/project/proj2.pl
+++ mc346/prolog/project/proj2.pl
@@ -1,26 +1,57 @@
 main :-
   prompt(_, ''),
-  read_string(user_input, _, Input),
-  toTrechos(Input, Trechos),
-  getSegments(Trechos, Segments),
+  % read_string(user_input, _, Input),
+  toTrechos(
+  "xxxxxababababyyyyyy
+yyaaaaaaaaaaa
+yyyyyyeeeeeeeeeeeeee
+cccccccccccccccxxxxx
+fffffffffffffffwwwwww
+wwwwwwgggggggggggxx", Trechos),
+  getSegments(Trechos, Trechos, Segments),
 
-  printList(Trechos),
-  halt.
+  printList(Trechos).
 
 % Print a list
 printList([]).
-printList([X|XS]) :- writeln(user_output, X), printList(XS).
+printList([X|XS]) :-
+  writeln(user_output, X),
+  printList(XS).
 
 % Take the string input, and return list of trechos
-toTrechos(Input, Trechos) :- atomic_list_concat(TrechosString, '\n', Input),  maplist(atom_chars, TrechosString, Trechos).
+toTrechos(Input, Trechos) :-
+  atomic_list_concat(TrechosString, '\n', Input),
+  maplist(atom_chars, TrechosString, Trechos).
 
 % Take the list of trechos, and return the segments
 getSegments([], _, []).
-getSegments([X|XS], Trechos, Segments) :- getSegments(XS, Trechos, SegmentsR), getXSegments(X, Trechos, MatchList), Segments is [MatchList|SegmentsR].
+getSegments([X|XS], Trechos, Segments) :-
+  getSegments(XS, Trechos, SegmentsR),
+  getXSegments(X, Trechos, MatchList),
+  Segments = [MatchList|SegmentsR].
 
 getXSegments(X, [], []).
-getXSegments(X, [Y|YS], MatchList) :- getXSegments(X, YS, MatchListR), matchTrecho(X, Y, Segment, 0, YInicio), Segment != [] -> MatchList is [Segment|MatchListR]; MatchListR.
+getXSegments(X, [Y|YS], MatchList) :-
+  getXSegments(X, YS, MatchListR),
+  matchTrecho(X, Y, Segment, 0, Y),
+  Segment != [-1] ->
+    MatchList = [Segment|MatchListR];
+    MatchList = MatchListR.
 
-matchTrecho([], YS, Segment, Acc, YInicio) :- Acc >= 4 -> Segment is YS; Segment is false.
-matchTrecho([X|XS], [Y|YS], Segment, Acc, YInicio) :- X = Y, Acc is Acc + 1, matchTrecho(XS, YS, SegmentR, Acc), SegmentR = false -> Segment is false; Segment is [X|SegmentR].
-matchTrecho([X|XS], [Y|YS], Segment, Acc, YInicio) :- Acc is 0, matchTrecho(XS, YInicio, SegmentR, Acc), SegmentR = false -> Segment is false; Segment is [X|SegmentR].
+matchTrecho([], YS, Segment, Acc, YInicio) :-
+  Acc >= 4 ->
+    Segment = YS;
+    Segment = [-1].
+matchTrecho([X|XS], [Y|YS], Segment, Acc, YInicio) :-
+  X = Y,
+  Acc is Acc + 1,
+  matchTrecho(XS, YS, SegmentR, Acc, YInicio),
+  SegmentR == [-1] ->
+    Segment = [-1];
+    Segment = [X|SegmentR].
+matchTrecho([X|XS], [Y|YS], Segment, Acc, YInicio) :-
+  Acc is 0,
+  matchTrecho(XS, YInicio, SegmentR, Acc, YInicio),
+  SegmentR == [-1] ->
+    Segment = [-1];
+    Segment = [X|SegmentR].
