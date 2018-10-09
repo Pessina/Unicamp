898c496620e90df1ba2ea0427d3742cf54cd8595
Felipe Sousa Pessina <ra171214@scooby.lab.ic.unicamp.br>
2018-10-09 09:32:01 -0300

    fix bug combinations generations

--- mc346/prolog/project/proj2.pl
+++ mc346/prolog/project/proj2.pl
@@ -3,17 +3,20 @@ main :-
   % read_string(user_input, _, Input),
   toTrechos(
   "xxxxxababababyyyyyy
-yyyyyyeeeeeeeeeeeeee", Trechos),
+yyaaaaaaaaaaa
+yyyyyyeeeeeeeeeeeeee
+cccccccccccccccxxxxx
+fffffffffffffffwwwwww
+wwwwwwgggggggggggxx", Trechos),
   getSegments(Trechos, Trechos, Segments),
-
- printList(Segments).
+  writeln(Segments).
+  %printList(Segments).
 
 
 % Print a list
 printList([]).
 printList([X|XS]) :-
-  % writeln(user_output, X),
-  writeln(X),
+  writeln(user_output, X),
   printList(XS).
 
 % Take the string input, and return list of trechos
@@ -26,17 +29,19 @@ getSegments([], _, []).
 getSegments([X|XS], Trechos, Segments) :-
   getSegments(XS, Trechos, SegmentsR),
   getXSegments(X, Trechos, MatchList),
-  Segments = [MatchList|SegmentsR].
+  (MatchList == [] ->
+    Segments = [X|SegmentsR];
+   	append(MatchList, SegmentsR, Segments)).
 
 getXSegments(_, [], []).
 getXSegments(X, [Y|YS], MatchList) :-
   getXSegments(X, YS, MatchListR),
-  write(X),
-  writeln(Y),
-  matchTrecho(X, Y, Segment, 0, Y),
-  compare(=, SegmentR, [-1]) ->
-    MatchList = MatchListR;
-	MatchList = [Segment|MatchListR].
+  (X == Y ->
+  	MatchList = MatchListR;
+    matchTrecho(X, Y, Segment, 0, Y),
+    (Segment == [-1] ->
+      MatchList = MatchListR;
+      MatchList = [Segment|MatchListR])).
 
 matchTrecho([], YS, Segment, Acc, _) :-
   Acc >= 4 ->
@@ -46,17 +51,12 @@ matchTrecho([X|XS], [Y|YS], Segment, Acc, YInicio) :-
   X == Y,
   AccP is Acc + 1,
   matchTrecho(XS, YS, SegmentR, AccP, YInicio),
-  write(X),
-  write(" "),
-  write(SegmentR),
-  write(" "),
-  writeln([X|SegmentR]),
-  compare(=, SegmentR, [-1]) ->
+  (SegmentR == [-1] ->
    	Segment = [-1];
-    Segment = [X|SegmentR].
+    Segment = [X|SegmentR]).
 matchTrecho([X|XS], _, Segment, _, YInicio) :-
   AccP is 0,
   matchTrecho(XS, YInicio, SegmentR, AccP, YInicio),
-  compare(=, SegmentR, [-1]) ->
-    Segment = [-1];
-    Segment = [X|SegmentR].
+ (SegmentR == [-1] ->
+   	Segment = [-1];
+    Segment = [X|SegmentR]).
