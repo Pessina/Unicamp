e8a7a38b605aed764cab966c312b56497346684f
Felipe Sousa Pessina <ra171214@rocha.lab.ic.unicamp.br>
2018-10-18 19:59:32 -0300

    add code to remove elements that make a trecho

--- mc346/prolog/project/proj2.pl
+++ mc346/prolog/project/proj2.pl
@@ -8,16 +8,31 @@ yyyyyyeeeeeeeeeeeeee
 cccccccccccccccxxxxx
 fffffffffffffffwwwwww
 wwwwwwgggggggggggxx", Trechos),
-  getSegments(Trechos, Trechos, Segments),
-  writeln(Segments).
-  %printList(Segments).
-
+  recursiveTrechos(Trechos, TrechosR),
+  printList(TrechosR).
 
 % Print a list
 printList([]).
 printList([X|XS]) :-
-  writeln(user_output, X),
+  atom_chars(A, X),
+  writeln(A),
   printList(XS).
+
+% Recursive search for new trechos, until no more trechos can be created
+recursiveTrechos(Trechos, TrechosR) :-
+    length(Trechos, LengthI),
+    getSegments(Trechos, Trechos, Segments, XR, YR),
+    (var(XR), var(YR) ->
+    	TrechosR3 = Trechos;
+    	delete(Trechos, XR, TrechosR1),
+    	delete(TrechosR1, YR, TrechosR2),
+   		append(Segments, TrechosR2, TrechosR3)),
+    length(TrechosR3, LengthF),
+
+    (LengthI =\= LengthF ->
+      recursiveTrechos(TrechosR3, TrechosR);
+      TrechosR = TrechosR3).
+
 
 % Take the string input, and return list of trechos
 toTrechos(Input, Trechos) :-
@@ -25,23 +40,27 @@ toTrechos(Input, Trechos) :-
   maplist(atom_chars, TrechosString, Trechos).
 
 % Take the list of trechos, and return the segments
-getSegments([], _, []).
-getSegments([X|XS], Trechos, Segments) :-
-  getSegments(XS, Trechos, SegmentsR),
-  getXSegments(X, Trechos, MatchList),
-  (MatchList == [] ->
-    Segments = [X|SegmentsR];
-   	append(MatchList, SegmentsR, Segments)).
+getSegments([], _, [], _, _).
+getSegments([X|XS], Trechos, Segments, XR, YR) :-
+  getSegments(XS, Trechos, SegmentsR, XR, YR),
+  getXSegments(X, Trechos, MatchList, Xr, Yr),
+  ((MatchList == []; SegmentsR \= []) -> % Segments is with element
+    Segments = SegmentsR;
+   	append(MatchList, SegmentsR, Segments),
+    XR = Xr,
+    YR = Yr).
 
-getXSegments(_, [], []).
-getXSegments(X, [Y|YS], MatchList) :-
-  getXSegments(X, YS, MatchListR),
+getXSegments(_, [], [], _, _).
+getXSegments(X, [Y|YS], MatchList, XR, YR) :-
+  getXSegments(X, YS, MatchListR, XR, YR),
   (X == Y ->
   	MatchList = MatchListR;
     matchTrecho(X, Y, Segment, 0, Y),
-    (Segment == [-1] ->
+    ((Segment == [-1]; MatchListR \= []) -> % Or segment is with element
       MatchList = MatchListR;
-      MatchList = [Segment|MatchListR])).
+      MatchList = [Segment|MatchListR],
+      XR = X,
+      YR = Y)).
 
 matchTrecho([], YS, Segment, Acc, _) :-
   Acc >= 4 ->
