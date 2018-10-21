4715faf7efb17a82cfa3a177ff9bff187bafb089
Felipe <fs.pessina@gmail.com>
2018-10-21 19:18:25 -0300

    add coments

--- mc346/prolog/project/proj2.pl
+++ mc346/prolog/project/proj2.pl
@@ -38,8 +38,10 @@ slice([_|Xs],I,K,Ys) :-
 % Recursive search for new trechos, until no more trechos can be created
 recursiveTrechos(Trechos, TrechosR) :-
     getSegments(Trechos, Trechos, Segments, XR, YR),
+    % Check if Segments is empty, if yes stops the recursion
     (Segments == [] ->
     	TrechosR = Trechos;
+      % Remove used trechos, and add segment
     	delete(Trechos, XR, TrechosR1),
     	delete(TrechosR1, YR, TrechosR2),
    		append(Segments, TrechosR2, TrechosR3),
@@ -50,34 +52,41 @@ getSegments([], _, [], _, _).
 getSegments([X|XS], Trechos, Segments, XR, YR) :-
   getSegments(XS, Trechos, SegmentsR, XR, YR),
   getXSegments(X, Trechos, MatchList, Xr, Yr),
+  % If X matched with some trecho and its the first Segment, add to list. Else just return
   ((MatchList == []; SegmentsR \= []) ->
     Segments = SegmentsR;
    	append(MatchList, SegmentsR, Segments),
     XR = Xr,
     YR = Yr).
 
-%Get combinations for X element from trechos
+% Try to combine X with some other Trechos
 getXSegments(_, [], [], _, _).
 getXSegments(X, [Y|YS], MatchList, XR, YR) :-
   getXSegments(X, YS, MatchListR, XR, YR),
+  % If elements are the same, just skip
   (X == Y ->
   	MatchList = MatchListR;
     matchTrecho(X, Y, Segment, 3, 0),
+    % If X match with Y, and the match list is empty add segment to list, set XR and YR matched elements. Else just return
     ((Segment == [-1]; MatchListR \= []) ->
       MatchList = MatchListR;
       MatchList = [Segment|MatchListR],
       XR = X,
       YR = Y)).
 
-% Try to make a Segment unifying two gens
+% Recursive try to find the greates match between X and Y, slicing the lists
 matchTrecho(X, Y, Segment, Acc, AccMax) :-
     length(X, Xlen),
     length(Y, Ylen),
+    % Check if the slice is not greater than the lists
     (Acc =< Ylen, Acc =< Xlen ->
+      % Reverse the list and get the end X
       reverse(X, XRev),
       slice(XRev, 1, Acc, XSlice),
       reverse(XSlice, XRevSlice),
+      % Get the beggining of Y
       slice(Y, 1, Acc, YSlice),
+      % Check if the end of X is equal to beggining of Y
    	  (XRevSlice == YSlice ->
           AccMaxP is Acc,
           AccP is Acc + 1,
@@ -85,6 +94,7 @@ matchTrecho(X, Y, Segment, Acc, AccMax) :-
           AccP is Acc + 1,
           AccMaxP is AccMax,
           matchTrecho(X, Y, Segment, AccP, AccMaxP));
+      % At the end of recursion, slice the trechos, and combine then by the greatest intersection
       (AccMax >= 4 ->
         length(Y, Ylen),
         AccMaxP is AccMax + 1,
