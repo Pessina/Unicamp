5390b6f5d06aae6d8b1064e1ac2e7782fc000c83
Felipe Sousa Pessina <ra171214@garfield.lab.ic.unicamp.br>
2018-11-01 14:36:36 -0300

    add list3 python

---
+++ mc346/python/list3.py
@@ -0,0 +1,62 @@
+import time
+from datetime import datetime
+
+def decorator_time(f):
+    def wrapper_time(*args):
+
+        time_begin = datetime.now()
+        x = f(*args)
+        time_end = datetime.now()
+
+        print ((time_end - time_begin).total_seconds())
+        return x
+
+    return wrapper_time
+
+def decorator_info (f):
+
+    def wrapper_info (*args):
+
+        print ("Calling {0} with Arguments = {1}, time = {2}".format(f.__name__, args, datetime.now()))
+        x = f(*args)
+        print ("Return = {0}".format(x))
+
+        return x
+    return wrapper_info
+
+class Memorize:
+    def __init__ (self, f):
+        self.dict_mem = {}
+        self.f = f
+
+    def decorator_memorize (self):
+         def wrapper_memorize(*args):
+             try:
+                 x = self.dict_mem[args]
+                 print ('hit dict')
+                 return x
+             except:
+                 print ('miss dict')
+                 x = self.f(*args)
+                 self.dict_mem[args] = x
+                 return x
+         return wrapper_memorize
+
+def decorator_log(f, arq):
+    def wrapper_log(*args):
+        with open(arq, 'a') as arq_open:
+            arq_open.write("Calling {0} with Arguments = {1}, time = {2}\n".format(f.__name__, args, datetime.now()))
+            x = f(*args)
+            return x
+    return wrapper_log
+
+def f (x, y):
+    return x * y
+
+x = decorator_log(f, 'file.txt')
+
+print (x(5, 10))
+print (x(10, 10))
+print (x(10, 10))
+print (x(5, 12))
+print (x(5, 0))
