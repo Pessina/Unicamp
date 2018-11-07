a318d8483811bd9b8bad8baac402986a9b25f0b3
Felipe Sousa Pessina <ra171214@silva.lab.ic.unicamp.br>
2018-11-07 18:29:24 -0200

    add code to calculate inconvenience'

--- mc346/python/project/project.py
+++ mc346/python/project/project.py
@@ -37,7 +37,7 @@ def getMaxEdgeValue (graph_input):
     return max_index + 1
 
 def createGraph (max_index, graph_input):
-    graph = np.zeros((max_index,max_index))
+    graph = np.zeros((max_index,max_index), dtype='f')
 
     for element in graph_input:
         element = element.split(' ')
@@ -55,8 +55,8 @@ def floydWarshallWithPathReconstruction (graph):
         for j in range(0,v):
             p[i,j] = i
             if (i != j and graph[i,j] == 0):
-                p[i,j] = -30000
-                graph[i,j] = 30000 # set zeros to any large number which is bigger then the longest way
+                p[i,j] = -float('Inf')
+                graph[i,j] = float('Inf') # set zeros to any large number which is bigger then the longest way
 
     for k in range(0,v):
         for i in range(0,v):
@@ -67,30 +67,110 @@ def floydWarshallWithPathReconstruction (graph):
 
     return graph, p
 
-def constructPath(p, i, j, path=[]):
+def constructPath(p, i, j, path):
     i,j = int(i), int(j)
     if(i==j):
       path.append(i)
-    elif(p[i,j] == -30000):
+    elif(p[i,j] == -float('Inf')):
       path = None
     else:
       constructPath(p, i, p[i,j], path);
       path.append(j)
     return path
 
-def uberPool(uber_input):
-    
+def calculateInconvenience (first, second, path_graph, path_next_graph):
+    first = first.split(' ')
+    second = second.split(' ')
+    path = []
+
+    # If the passenger is on travel
+    # We use calculate the alredy made path, and use the remaining path to
+    # calculate inconvenience
+    if len(first) == 3:
+        path += constructPath(path_next_graph, first[0], first[2], [])
+        first[0] = first[2]
+
+    # Assing value to the path edges
+    A = int(first [0])
+    B = int(first [1])
+    C = int(second [0])
+    D = int(second [1])
+
+    # We assume that we have a possible path between A and B
+    # It means travel time is no inf
+    travel_time = path_graph[A][B]
+
+    # Possible paths A C B D or A C D B
+    time_with_passenger_1 = path_graph[A][C] + path_graph[C][B] + path_graph[B][D]
+    time_with_passenger_2 = path_graph[A][C] + path_graph[C][D] + path_graph[D][B]
+
+    inconvenience_1 = time_with_passenger_1/travel_time
+    inconvenience_2 = time_with_passenger_2/travel_time
+
+    # Return the smaller inconvenience if some of than is smaller than 1.4
+    if inconvenience_1 < inconvenience_2:
+        path += constructPath(path_next_graph, A, C, [])
+        path += constructPath(path_next_graph, C, B, [])[1:]
+        path += constructPath(path_next_graph, B, D, [])[1:]
+        return inconvenience_1, path
+    else:
+        if inconvenience_2 > 1.4:
+            return 1.5, []
+        else:
+            path += constructPath(path_next_graph, A, C, [])
+            path += constructPath(path_next_graph, C, D, [])[1:]
+            path += constructPath(path_next_graph, D, B, [])[1:]
+            return inconvenience_2, path
+
+
+def uberPool(uber_input, min_path_graph, min_path_next_edge_graph):
+    min_inconvenience = 1.4
+    best_path = []
+    passenger1 = None
+    passenger2 = None
+    for first_passenger in uber_input:
+        for second_passenger in uber_input:
+            if len(second_passenger.split(' ')) == 3 or first_passenger == second_passenger:
+                continue
+            else:
+                inconvenience, path = calculateInconvenience (first_passenger, second_passenger, min_path_graph, min_path_next_edge_graph)
+                if inconvenience < min_inconvenience:
+                    passenger1 = first_passenger
+                    passenger2 = second_passenger
+                    min_inconvenience = inconvenience
+                    best_path = path
+
+    if best_path != []:
+        uber_input.remove(passenger1)
+        uber_input.remove(passenger2)
+
+    return best_path, uber_input
 
 graph_input, uber_input = read_input()
 graph = createGraph(getMaxEdgeValue(graph_input), graph_input)
-minPathGraph, minPathNextEdgeGraph = floydWarshallWithPathReconstruction(graph)
-path = constructPath(minPathNextEdgeGraph,0,2)
+min_path_graph, min_path_next_edge_graph = floydWarshallWithPathReconstruction(graph)
 
+path_list = []
 
+# Calculate the pair with inconvenience smaller than 1.4
+while True:
+    path, uber_input = uberPool(uber_input, min_path_graph, min_path_next_edge_graph)
+    if (path != []):
+        path_list.append(path)
+    else:
+        break
 
+# Append other paths, without pairs
+if (uber_input != []):
+    for element in uber_input:
+        element = element.split(' ')
+        path = []
+        if len(element == 3):
+            path += constructPath(path_next_graph, element[0], element[2], [])
+            path += constructPath(path_next_graph, element[2], element[1], [])
+            path_list.append(path)
+        else:
+            path += constructPath(path_next_graph, element[0], element[1], [])
+            path_list.append(path)
 
-
-
-
-
-#
+print (path_list)
