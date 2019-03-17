d60c63a12c340fba8ebd81663f6ee5fb4ff62871
Pessina <fs.pessina@gmail.com>
2019-03-17 08:53:21 -0300

    finish project

--- mc920/trabalho-0/trabalho.py
+++ mc920/trabalho-0/trabalho.py
@@ -3,7 +3,12 @@ import cv2
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction import image
 
+# Leitura das imagens
 img = cv2.imread('./images/baboon.png', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('./images/butterfly.png', cv2.IMREAD_GRAYSCALE)
+
+# Imagem auxiliar
+vis = np.zeros((512, 512), np.uint8)
 
 # Alterar brilho da imagem pelo lambda
 
@@ -20,6 +25,7 @@ img = cv2.imread('./images/baboon.png', cv2.IMREAD_GRAYSCALE)
 
 # Planos de bit da imagem\
 
+# Mascara para extracao do plano
 # def bit_plane (x):
 #     plane = int('00000001', 2)
 #     x = x & plane
@@ -30,15 +36,35 @@ img = cv2.imread('./images/baboon.png', cv2.IMREAD_GRAYSCALE)
 
 # Criando mosaico
 
-h = w = 128
-mosaic = np.empty((128,128), float)
+# h = w = 128
+# mosaic = []
+#
+# Crop da imagem, em blocos de 128x128
+# for y in range (0, 512, 128):
+#     for x in range (0, 512, 128):
+#         mosaic.append(img[y:y+h, x:x+w])
 
-for x in range (0, 512, 128):
-    for y in range (0, 512, 128):
-        mosaic = np.append(mosaic, img[y:y+h, x:x+w])
+# Reordenacao dos blocos do mosaico
+# indexList = [5, 10, 12, 2,
+#             7, 15, 0, 8,
+#             11, 13, 1, 6,
+#             3, 14, 9, 4]
+#
+# mosaic = [mosaic[i] for i in indexList]
 
-print (mosaic.shape)
-plt.imshow(mosaic[2], cmap="gray")
+# Reconstrucao da imagem 512x512
+# for i in range (0, len(mosaic)):
+#     y = (i/4) * 128
+#     x = (i % 4) *128
+#     vis[y:y+h, x:x+w] = mosaic[i]
+
+# Combinacao de imagens
+
+# vis = img * 0.2 + img2 * 0.8
+
+# Print matplotlib
+
+plt.imshow(vis, cmap="gray")
 plt.show()
 
 # Print com opencv
