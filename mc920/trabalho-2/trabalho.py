3da3a4933a67236a80e167ff52bae2f41e8f0016
Felipe Sousa Pessina <ra171214@saocristovao.lab.ic.unicamp.br>
2019-05-08 12:28:34 -0300

    update projetct mc920

--- mc920/trabalho-2/trabalho.py
+++ mc920/trabalho-2/trabalho.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-def halft_pontilhado(img):
+def halft_ordenado(img):
 	(row, col) = img.shape
 	img = np.floor((10/255) * img)
 	img_out = np.zeros((row * 3, col *3))
@@ -14,16 +14,15 @@ def halft_pontilhado(img):
 		for j in range (0, col):
 			for x in range (0, 3):
 				for y in range(0, 3):
-					img_out [3 * i + x, 3 * j + y] = int(img[i, j] >= mask[x, y])
+					img_out [3 * i + x, 3 * j + y] = int(img[i, j] > mask[x, y])
 	return img_out
 
-def halft_floyd_steinberg(img):
+def halft_floyd_steinberg(img, alternating=True):
 	(row, col) = img.shape
 	tmp = np.zeros((row, col))
 	for i in range(0, row - 1):
-		if i%2 == 0:
-			var = range (0, col - 1)
-		else:
+		var = range (0, col - 1)
+		if i%2 == 0 and alternating:
 			var = range (col - 2, -1, -1)
 		for j in var:
 			if img[i, j] < 128:
@@ -37,10 +36,18 @@ def halft_floyd_steinberg(img):
 			img[i+1, j+1] = img[i+1, j+1] + (1/16) * erro
 	return tmp
 
-images = ['baboon.pgm']
+images = ['baboon', 'fiducial', 'monarch', 'peppers', 'retina', 'sonnet', 'wedge']
 
 for i in range(0, len(images)):
-	img = cv2.imread('images/' + images[i], -1)
-#	img = halft_pontilhado(img)
-	img = halft_floyd_steinberg(img)
-	cv2.imwrite('baboonn.pbm', img)
+	img = cv2.imread('images/' + images[i] + '.pgm', -1)
+	img_ordenado = halft_ordenado(img)
+
+	img = cv2.imread('images/' + images[i] + '.pgm', -1)
+	img_floyd_steinberg = halft_floyd_steinberg(img, False)
+
+	img = cv2.imread('images/' + images[i] + '.pgm', -1)
+	img_floyd_steinberg_alternando = halft_floyd_steinberg(img)
+
+	cv2.imwrite('final_images/' + images[i] + '_ordenado' + '.pbm', img_ordenado)
+	cv2.imwrite('final_images/' + images[i] + '_floyd_steinberg' + '.pbm', img_floyd_steinberg)
+	cv2.imwrite('final_images/' + images[i] + '_floyd_steinberg_alternando' + '.pbm', img_floyd_steinberg_alternando)
