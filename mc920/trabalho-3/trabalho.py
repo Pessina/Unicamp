c5d0950f55aa3eead1eda6323fb0b093247ded0b
Pessina <fs.pessina@gmail.com>
2019-05-22 09:47:57 -0300

    update code trab 3

--- mc920/trabalho-3/trabalho.py
+++ mc920/trabalho-3/trabalho.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+# Apply dilatation, erosion, bitwise_and and closing to the image
 def morphological_operations (inverse, k1, k2, k3):
     kernel1 = np.ones((1,k1),np.uint8)
     dilation1 = cv2.dilate(inverse,kernel1,iterations = 1)
@@ -18,43 +19,51 @@ def morphological_operations (inverse, k1, k2, k3):
     connectivity = 4
     return cv2.connectedComponentsWithStats(closing, connectivity, cv2.CV_32S)
 
+def calculate_ratios (rectangle, width, height):
+    # Ratio black and white
+    total_blacks_dots = np.sum(rectangle)
+    ratio_black_to_white = total_blacks_dots/(width * height)
+
+    # Ratio transitions
+    total_transitions = 0
+    for i in range (0, height):
+        total_transitions += (rectangle[i, :-1] < rectangle[i, 1:]).sum()
+    for i in range (0, width):
+        total_transitions += (rectangle[:-1, i] < rectangle[1:, i]).sum()
+
+    if total_transitions != 0:
+        ratio_transitions = total_transitions/total_blacks_dots
+    else:
+        ratio_transitions = 0
+
+    return ratio_black_to_white, ratio_transitions
+
 # Read image
 img = cv2.imread("images/bitmap.pbm", -1)
-
 # Convert to binary image
 img[img == 255] = 1
 
-# Invert image to apply morphological operations
+# Invert image, 1 is black and 0 is white
 inverse = np.ones(img.shape, np.uint8)
 inverse [img == 1] = 0
 
 output1 = morphological_operations (inverse, 100, 200, 30)
-
 for x1, y1, width1, height1, area1 in output1[2]:
-        # Extract rectangle to calculate ratio
-        rectangle = inverse[y1:y1+height1, x1:x1+width1]
+        # Extract rectangle of the line to calculate ratio
+        rectangle_line = inverse[y1:y1+height1, x1:x1+width1]
+        ratio_black_to_white, ratio_transitions = calculate_ratios(rectangle_line, width1, height1)
 
-        # Ratio black and white
-        total_blacks_dots = np.sum(rectangle)
-        ratio_black_to_white = total_blacks_dots/(width1 * height1)
-
-        # Ratio transitions
-        total_transition = 0
-        for i in range (0, height1):
-            total_transition += (rectangle[i, :-1] < rectangle[i, 1:]).sum()
-        for i in range (0, width1):
-            total_transition += (rectangle[:-1, i] < rectangle[1:, i]).sum()
-
-        if total_transition != 0:
-            ratio_transition = total_transition/total_blacks_dots
-        else:
-            ratio_transition = 0
-
-        # Applying morphological operations on each line
-        if ratio_black_to_white > 0.2 and ratio_black_to_white < 0.5 and ratio_transition > 0.25:
-                output2 = morphological_operations (rectangle, 10, 10, 13)
-                # Drawing rectangles
+        # Filtering what is text and not text
+        if ratio_black_to_white > 0.2 and ratio_black_to_white < 0.5 and ratio_transitions > 0.25:
+                output2 = morphological_operations (rectangle_line, 10, 10, 13)
                 for x2, y2, width2, height2, area2 in output2[2][1:]:
-                    cv2.rectangle(inverse, (x1 + x2, y1 + y2), (x1 + x2 + width2, y1 + y2 + height2), 1, 1)
+                    rectangle_word = inverse[y1+y2:y1+y2+height2, x1+x2:x1+x2+width2]
+                    ratio_black_to_white, ratio_transitions = calculate_ratios(rectangle_word, width2, height2)
+                    # Filtering what is word and not
+                    if ratio_black_to_white > 0.1 and ratio_black_to_white < 0.7:
+                        # Drawing rectangles
+                        cv2.rectangle(inverse, (x1 + x2, y1 + y2), (x1 + x2 + width2, y1 + y2 + height2), 1, 1)
 
-cv2.imwrite("image.pbm", inverse)
+final = np.zeros(img.shape, np.uint8)
+final [inverse == 0] = 1
+cv2.imwrite("image.pbm", final)
